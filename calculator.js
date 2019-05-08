4fe39c7702ed241971b6dde65901bb17a1b45c60
Mhrpatel85 <29385964+Mhr85@users.noreply.github.com>
2019-05-08 16:38:27 -0400

    built bmi calculator with express & node

--- calculator.js
+++ calculator.js
@@ -15,6 +15,22 @@ app.post('/', function(req, res) {
   res.send('Result: ' + result);
 })
 
+//BMI CALCULATOR CODE
+app.get('/bmicalculator', function(req, res) {
+  res.sendFile(__dirname + '/bmicalculator.html');
+});
+
+app.post('/bmicalculator', function(req, res) {
+  var weight = parseFloat(req.body.weight);
+  var height = parseFloat(req.body.height);
+
+  var bmi = (weight / (height * height));
+
+  res.send('Your BMI: ' + bmi);
+});
+
+
+
 app.listen(3000, function() {
   console.log('Server started on port 3000');
 });
